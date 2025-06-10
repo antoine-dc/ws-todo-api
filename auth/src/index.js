f9ea9b71463b343b4a003835c1a3de9cea90cf66
Antoine DC <antoinedeconto@gmail.com>
2025-06-10 12:38:07 +0200

    I.III – Première exécution

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,3 +1,11 @@
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
